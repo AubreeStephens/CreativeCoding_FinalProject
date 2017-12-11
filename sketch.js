a7ed31d1ca7f82c408b7c8c5e0e0b5e0f518439d
Aubree Stephens <ajs890@nyu.edu>
2017-12-11 18:09:24 -0500

    Push_ Home Screen

--- sketch.js
+++ sketch.js
@@ -1,7 +1,50 @@
-function setup() {
-  // put setup code here
+var img, img1;
+var cnv;
+
+function preload(){
+img= loadImage('data/home_italy.png');
+img1=loadImage('data/see_tuscany_10.jpg');
 }
 
-function draw() {
-  // put drawing code here
+function setup(){
+
+cnv=createCanvas(windowWidth, windowHeight);
+  centerCanvas();
+ background(img1);
+//createCanvas(windowWidth, windowHeight);
+imageMode(CENTER);// Allows image to be centered on screen
+//image(img, windowWidth/2, windowHeight/2);
+
+}
+
+function draw(){
+
+	// crosshair tool for cities
+//background(img1);
+image(img, windowWidth/2, windowHeight/2);
+stroke(0);
+strokeWeight(1); // thickness
+
+  line(0, mouseY, width, mouseY);
+  line(mouseX, 0, mouseX, height);
+
+  print("Mouse X is:  "); 
+  print(mouseX);
+  print(" Mouse Y is: ");
+  print(mouseY);
+
+ellipseMode(CENTER);
+  ellipse (511, 41, 10, 10);//Milan
+  fill(0);
+}
+
+function centerCanvas() { //centers canvas 
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+
+function windowResized() {
+  centerCanvas();
 }
