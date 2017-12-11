2dcdc4d6ae42815cb186426da802437806b449cf
Aubree Stephens <ajs890@nyu.edu>
2017-12-11 18:53:12 -0500

    Push_City Dots

--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ img1=loadImage('data/see_tuscany_10.jpg');
 
 function setup(){
 
-cnv=createCanvas(windowWidth, windowHeight);
+cnv=createCanvas(1714, 1714);
   centerCanvas();
  background(img1);
 //createCanvas(windowWidth, windowHeight);
 imageMode(CENTER);// Allows image to be centered on screen
-//image(img, windowWidth/2, windowHeight/2);
+//image(img, width/2, height/2);
 
 }
 
@@ -21,12 +21,12 @@ function draw(){
 
 	// crosshair tool for cities
 //background(img1);
-image(img, windowWidth/2, windowHeight/2);
+image(img, width/2, height/2);
 stroke(0);
 strokeWeight(1); // thickness
 
-  line(0, mouseY, width, mouseY);
-  line(mouseX, 0, mouseX, height);
+  //line(0, mouseY, width, mouseY);
+  //line(mouseX, 0, mouseX, height);
 
   print("Mouse X is:  "); 
   print(mouseX);
@@ -34,8 +34,12 @@ strokeWeight(1); // thickness
   print(mouseY);
 
 ellipseMode(CENTER);
-  ellipse (511, 41, 10, 10);//Milan
+  ellipse (700, 675, 10, 10);//Milan
   fill(0);
+
+ellipse (725, 765, 10, 10);//Cinque Terre
+ellipse (795, 775, 10, 10);//Firenze
+ellipse (840, 825, 10, 10);//Montepulciano
 }
 
 function centerCanvas() { //centers canvas 
